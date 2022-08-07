63a19e9e67ebf3decc271c0b2e39ba8c62c046e9
Adan G. Medrano-Chavez <ryuuba@gmail.com>
2022-08-07 15:45:15 -0500

    Back up a value

--- q10.s
+++ q10.s
@@ -3,7 +3,7 @@
 # int b = a % 4 < 2;
 # s1 <-> a, s2 <-> b
         lui  s1, 0x00002
-        addi s1, s1, 0xFFFFF85C
+        addi s1, s1, 0xFFFFF85A
         andi t0, s1, 0x003
         slti s2, t0, 2
         nop
